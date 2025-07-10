f3e6098906fe6dcbbb867e42a363282893f7da28
Ahmed Mohsen <ahmedthemohsen@gmail.com>
2025-07-10 22:07:35 +0300

    managed login and signup states

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/firebase_options.dart';
 import 'package:flutter_chat/views/auth_view.dart';
+import 'package:flutter_chat/views/chat_view.dart';
+import 'package:flutter_chat/views/splash_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +25,18 @@ class App extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 63, 17, 177),
         ),
       ),
-      home: AuthView(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashView();
+          }
+          if (snapshot.hasData) {
+            return const ChatView();
+          }
+          return const AuthView();
+        },
+      ),
     );
   }
 }
---
+++ lib/views/chat_view.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatView extends StatelessWidget {
+  const ChatView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter chat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.logout,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+      body: Center(child: Text('Welcome')),
+    );
+  }
+}
